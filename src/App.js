185fbfd7ca3cfdfb9fe60d333ee8110407021153
Rocketman <abhinayshambharkar1@gmail.com>
2025-03-24 16:00:59 +0530

    final fix

--- src/App.js
+++ src/App.js
@@ -1,76 +1,63 @@
-import React, { useEffect, useState } from "react";
-import "./styles.css";
+import React, { useEffect, useState } from 'react'
+import './styles.css'
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(1);
 
-  const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products?limit=100");
-    const data = await res.json();
-    if (data && data.products) {
+  const[products,setProducts] = useState([]);
+  const[page,setPage] = useState(1);
+
+  const fetchProducts = async () =>{
+    const response = await fetch('https://dummyjson.com/products?limit=100');
+    const data = await response.json();
+    console.log(data);
+    if(data && data.products && data.products.length){
       setProducts(data.products);
     }
-  };
+  }
 
-  console.log(products);
-
-  useEffect(() => {
+  useEffect(()=>{
     fetchProducts();
-  }, []);
+  },[])
 
-  const selectPageHandler = (selectedPage) => {
-    if (
-      selectedPage >= 1 &&
-      selectedPage <= products.length &&
-      selectedPage !== page
-    )
-      setPage(selectedPage);
-  };
+  const selectedPageHandler = (selectedPage) =>{
+    if(selectedPage>=1 && selectedPage<=products.length/10 && selectedPage!==page)
+    setPage(selectedPage);
+  }
 
   return (
     <div>
-      {products.length > 0 && (
-        <div className="products">
-          {products.slice(page * 10 - 10, page * 10).map((item) => {
-            return (
-              <span className="products__single" key={item.id}>
-                <img src={item.thumbnail} alt={item.thumbanil} />
+      {
+        (products && products.length) && <div className='products'>
+          {
+            products.slice(page*10-10,page*10).map((item) => {
+              return <span className='products__single' key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
                 <span>{item.title}</span>
               </span>
-            );
-          })}
+            }) 
+          }
         </div>
-      )}
-      {products.length > 0 && (
-        <div className="pagination">
-          <span
-            onClick={() => selectPageHandler(page - 1)}
-            className={page > 1 ? "" : "pagination__disable"}
-          >
-            👈
-          </span>
-          {[...Array(products.length / 10)].map((_, ind) => {
-            return (
-              <span
-                className={page === ind + 1 ? "pagination__selected" : ""}
-                onClick={() => selectPageHandler(ind + 1)}
-                key={ind}
-              >
-                {ind + 1}
-              </span>
-            );
-          })}
-          <span
-            onClick={() => selectPageHandler(page + 1)}
-            className={page < products.length / 10 ? "" : "pagination__disable"}
-          >
-            👉
-          </span>
+      }{
+        (products && products.length) && <div className='pagination'>
+          <span onClick={()=>selectedPageHandler(page-1)}
+            className={page>1?"":"pagination__disable"}
+          >◀️</span>
+          {
+            [...Array(products.length/10)].map((_,i)=>{
+              return <span
+                      className={page===i+1?"pagination__selected":""}
+                      onClick={()=>selectedPageHandler(i+1)} 
+                      key={i}
+                      >{i+1}</span>
+            })
+          }
+          <span onClick={()=>selectedPageHandler(page+1)}
+            className={page<products.length/10?"":"pagination__disable"}
+            >▶️</span>
         </div>
-      )}
+      }
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
